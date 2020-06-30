7c702ca1edef3018c7f04c22f0bd106827b63375
aritche <ari.tchetchenian@gmail.com>
2020-07-01 01:26:09 +1000

    batched data

--- IO/preprocess.py
+++ IO/preprocess.py
@@ -5,7 +5,73 @@ from tractseg.libs import data_utils # assumes tractseg is installed
 
 import nibabel as nib
 import numpy as np
+from glob import glob
+import os
 
+
+#fn = '../../DATASETS/HCP_100_SUBJECTS/100307/T1w/Diffusion/nodif_brain_mask.nii.gz' # nodif_brain_mask
+base_dir = '../../DATASETS/TRACTSEG_105_SUBJECTS'
+subjects = [subject.split('/')[-1] for subject in glob(base_dir + '/hcp_brain_masks/*')]
+
+for subject in subjects:
+    # Compute the bounding boxes for each tract
+    bbox_dir = base_dir + '/generated_tract_masks/' + subject
+    print(subject)
+
+    # Base directories for output
+    b1 = base_dir + '/preprocessed/generated_tract_masks/' + subject
+    b2 = base_dir + '/preprocessed/generated_endings_masks/' + subject
+    b3 = base_dir + '/preprocessed/generated_toms/' + subject
+    for base in [b1, b2, b3]:
+        os.mkdir(base) # make the output subject directory
+
+    for tract_fn in glob(bbox_dir + '/*'):
+        # Load the mask file and compute bounding box
+        mask = nib.load(tract_fn).get_data()
+        if np.sum(mask) != 0:
+            bbox = data_utils.get_bbox_from_mask(np.nan_to_num(mask), 0)
+        else:
+            bbox = [[0,mask.shape[0]], [0,mask.shape[1]], [0,mask.shape[2]]]
+
+        tract_name = tract_fn.split('/')[-1].split('.')[0]
+
+        # Input directories
+        f1 = base_dir + '/generated_tract_masks/' + subject + '/' +  tract_name + '.trk.nii.gz'
+        f2 = base_dir + '/generated_endings_masks/' + subject + '/' +  tract_name + '_beginnings.nii.gz'
+        f3 = base_dir + '/generated_endings_masks/' + subject + '/' +  tract_name + '_endings.nii.gz'
+        f4 = base_dir + '/generated_toms/' + subject + '/' +  tract_name + '_DIRECTIONS.nii.gz'
+        
+        # Output directories
+        o1 = base_dir + '/preprocessed/generated_tract_masks/' + subject + '/' +  tract_name + '.trk.nii.gz'
+        o2 = base_dir + '/preprocessed/generated_endings_masks/' + subject + '/' +  tract_name + '_beginnings.nii.gz'
+        o3 = base_dir + '/preprocessed/generated_endings_masks/' + subject + '/' +  tract_name + '_endings.nii.gz'
+        o4 = base_dir + '/preprocessed/generated_toms/' + subject + '/' +  tract_name + '_DIRECTIONS.nii.gz'
+
+
+        fns = [f1, f2, f3, f4]
+        outs = [o1, o2, o3, o4]
+
+        i = 0
+        for fn in fns:
+            # Load the file you are interested in preprocessing
+            img = nib.load(fn)
+            affine = img.affine
+            data = img.get_data()
+            
+            # Adjust the data to have 4 dimensions, since the cropping method will iterate over this fourth dim
+            if len(data.shape) == 3:
+                data = data[..., None]
+
+            # Crop and pad to create a square volume of size 144 x 144 x 144
+            data, _, _, original_shape = data_utils.crop_to_nonzero(np.nan_to_num(data), bbox=bbox)
+            data, transform_applied = data_utils.pad_and_scale_img_to_square_img(data)
+
+            # Save the result
+            out_dir = outs[i]
+            nib.save(nib.Nifti1Image(data, affine), out_dir)
+             
+            i += 1
+"""
 # Load the nodif_brain_mask and get the bounding box of the brain region
 mask = nib.load('../../DATASETS/TRACTSEG_105_SUBJECTS/hcp_brain_masks/672756/nodif_brain_mask.nii.gz').get_data()
 bbox = data_utils.get_bbox_from_mask(np.nan_to_num(mask), 0)
@@ -14,7 +80,7 @@ print(bbox)
 # Load the file you are interested in preprocessing
 #img = nib.load('../../DATASETS/tractseg_output_672756/TOM/CST_left.nii.gz')
 #img = nib.load('../../DATASETS/TRACTSEG_105_SUBJECTS/generated_toms/672756/CST_left_DIRECTIONS.nii.gz')
-img = nib.load('../../DATASETS/TRACTSEG_105_SUBJECTS/generated_tract_masks/672756/CST_left.trk.nii.gz')
+#img = nib.load('../../DATASETS/TRACTSEG_105_SUBJECTS/generated_tract_masks/672756/CST_left.trk.nii.gz')
 affine = img.affine
 data = img.get_data()
 
@@ -41,39 +107,4 @@ original = data_utils.cut_and_scale_img_back_to_original_img(data, transform_app
 original = data_utils.add_original_zero_padding_again(original, bbox, original_shape, nr_of_classes=1)
 print(original.shape)
 nib.save(nib.Nifti1Image(original, affine), "original.nii.gz")
-
-"""
-#fn = '../../DATASETS/HCP_100_SUBJECTS/100307/T1w/Diffusion/nodif_brain_mask.nii.gz' # nodif_brain_mask
-base_dir = '../../DATASETS/TRACTSEG_105_SUBJECTS'
-subjects = [subject.split('/')[-1] for subject in glob(base_dir + '/hcp_brain_masks/*')]
-
-for subject in subjects:
-    # Compute the bounding box from the whole brain mask
-    mask_fn = base_dir + '/hcp_brain_masks/' + subject + '/nodif_brain_mask.nii.gz'
-    data = nib.load(mask_fn).get_data()
-    bbox = get_bbox_from_mask(np.nan_to_num(data), 0)
-
-    for file_type in ['generated_tract_masks', 'generated_toms', 'generated_endings_masks']:
-        fn = base_dir + '/' + file_type + '/' + subject + '/*'
-        for tract_fn in glob(fn):
-            # Load the file
-            img = nib.load(tract_fn)
-            data = img.get_data()
-            affine = img.affine
-            data = np.nan_to_num(data)
-
-            # Add channel dimension if does not exist yet
-            if len(data.shape) == 3:
-                data = data[..., None]
-
-            # Crop using the existing bounding box
-            data = crop_to_nonzero(data, bbox=bbox)
-            data, _ = pad_and_scale_img_to_square_img(data)
-            print(data.shape)
-
-            # Write to file
-            nib.save(nib.Nifti1Image(data, affine), "test_" + tract_fn.split('/')[-1])
-            break
-        break
-    break
 """
