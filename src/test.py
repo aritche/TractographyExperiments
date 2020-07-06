808265f5f6605ef474f4a8ae246f16b6480fd605
aritche <ari.tchetchenian@gmail.com>
2020-07-06 19:22:42 +1000

    new model and added keyword for trainer to speed up prototyping

--- src/test.py
+++ src/test.py
@@ -4,7 +4,7 @@ Script for testing a model
 
 import torch
 import numpy as np
-from models.single_tract import CustomDataset
+from models.single_tract_3d import CustomDataset
 from dipy.io.streamline import load_trk, save_trk
 import matplotlib.pyplot as plt
 
@@ -15,29 +15,48 @@ def save_as_trk(output, fn):
     output = np.reshape(output, (1024,100,3))
 
     ref_trk = load_trk('../../DATASETS/TRACTSEG_105_SUBJECTS/tractograms/672756/tracts/CST_left.trk', 'same', bbox_valid_check=False)
+
     sls = []
     for item in output:
         sls.append(item)
     ref_trk.streamlines = sls
-
-
     save_trk(ref_trk, fn + '.trk', bbox_valid_check=False)
-        
 
+    plt.hist(output.ravel())
+    plt.show()
+
+    return output
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-model = torch.load('epoch_300.pth')
+model_name = input("Model name:")
+epoch_number = input("Epoch number:")
+test_dir = input("Test Directory:")
+test_dir = "../data/" + test_dir
+
+# Load model
+fn = './results/' + model_name + '/epoch_' + epoch_number + '.pth'
+model = torch.load(fn)
 model.to(device)
-dataset = CustomDataset('../data/test/CST_TOMs', '../data/test/CST_endings_masks', '../data/CST_tractograms')
+
+# Load dataset
+dataset = CustomDataset(test_dir + '/CST_TOMs', test_dir + '/CST_endings_masks', test_dir + '/CST_tractograms')
 testloader = torch.utils.data.DataLoader(dataset, batch_size=1)
 
 torch.cuda.empty_cache()
 i = 0
+results = []
 for inputs, labels in testloader:
+    TEMP = labels[0].cpu().detach().numpy()   
+    plt.hist(TEMP.ravel())
+    plt.show()
+
     inputs, labels = inputs.to(device), labels.to(device)
     output = model.forward(inputs)
 
-    save_as_trk(output, str(i))
+    result = save_as_trk(output, str(i))
+    results.append(result)
     i += 1
+
+print(np.sum(np.abs(results[0] - results[1])))
--- src/train.py
+++ src/train.py
@@ -4,7 +4,8 @@ Script for training a model
 import numpy as np
 import os
 
-from models.single_tract import CustomDataset, CustomModel, CustomLoss
+from models.cst_left_3d import CustomDataset, CustomModel, CustomLoss
+#from models.single_tract import CustomDataset, CustomModel, CustomLoss
 
 from resources.vis import VisdomLinePlotter
 
@@ -17,10 +18,11 @@ from torchsummary import summary
 Hyperparameters
 """
 EPOCHS = 500
-BATCH_SIZE = 32
+BATCH_SIZE = 16
 LR = 10e-4
-VALID_SPLIT = 0.2
+VALID_SPLIT = 0.15
 np.random.seed(66)
+torch.manual_seed(66)
 
 
 """
@@ -33,7 +35,7 @@ plotter = VisdomLinePlotter(env_name='HairNet Training Experiment')
 Load the data
 """
 print('Loading data...')
-dataset = CustomDataset('../data/CST_TOMs', '../data/CST_endings_masks', '../data/CST_tractograms')
+dataset = CustomDataset('../data/CST_TOMs', '../data/CST_endings_masks', '../data/CST_tractograms_raw')
 
 # Split into training/validation (https://stackoverflow.com/a/50544887)
 indices = list(range(len(dataset)))
@@ -58,13 +60,15 @@ model = CustomModel()
 model.to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=LR)
 
-summary(model, (3, 256, 256))
+#summary(model, (3, 256, 256))
+summary(model, (3, 144, 144, 144))
 
 # Create the results directory
-model_name = input('** Name for the model (no spaces):')
-while os.path.exists('./results/' + model_name):
-    model_name = input('Model already exists. Please enter another name:')
-os.mkdir('./results/' + model_name) 
+model_name = input('** Name for the model (no spaces) [enter "dump" for no results]:')
+if model_name != "dump":
+    while os.path.exists('./results/' + model_name):
+        model_name = input('Model already exists. Please enter another name:')
+    os.mkdir('./results/' + model_name) 
 
 
 print("Training...")
@@ -107,7 +111,7 @@ for epoch in range(EPOCHS):
 
     plotter.plot('loss per item', 'validation', 'Results', epoch, valid_loss/valid_items)
 
-    print("Epoch %d/%d:\t%.5f\t%.5f" % (epoch, EPOCHS, train_loss, valid_loss))
+    print("Epoch %d/%d:\t%.5f\t%.5f" % (epoch, EPOCHS, train_loss/train_items, valid_loss/valid_items))
 
     if epoch % 100 == 0:
         print('Saving intermediate model...')
