a1811203ea80465685e105a94aa22063c9a5d423
aritche <ari.tchetchenian@gmail.com>
2020-07-17 03:15:55 +1000

    more steps

--- resources/generating_dataset/generate_dataset.sh
+++ resources/generating_dataset/generate_dataset.sh
@@ -1,8 +1,12 @@
+echo "THIS PROGRAM ONLY WORKS FOR TRACTSEG TRACTOGRAMS <=V1.1.0. You need to change the way some of the helper files open .trk files to use this script on more recent tractograms."
+echo "Press any key to confirm..."
+read dump
+
 # Locate all of the relevant scripts
 subsample_script="./subsample_tractograms.py"
 generate_masks="./tractseg_helper_scripts/trk_2_binary.py"
 generate_endings="./tractseg_helper_scripts/create_endpoints_mask_with_clustering.py"
-generate_toms=""
+generate_toms="../../../../Programs/MITK-Diffusion-Ubuntu/MitkFiberDirectionExtraction.sh"
 
 input_base="../../../DATASETS/V1.1.0_TRACTSEG_105_SUBJECTS_V1.1.0"
 
@@ -34,20 +38,23 @@ do
 done
 
 
-#for trk_fn in $output_dir xxx:
-    ### GENERATE A TRACT MASK ###
-    # Since all HCP data shares same affine, can use any subject as the affine reference
-    #ref_affine_file="../../../DATASETS/HCP_100_SUBJECTS/100307/T1w/Diffusion/nodif_brain_mask.nii.gz" 
-    #output_dir=
-    #is_legacy=1 # set to 1 if using legacy data (dataset <=V1.1.0), else set to 0
+for trk_name in `ls -1 $output_trk_base | sed 's/\.trk$//g'`
+do
+    trk_fn="${output_trk_base}/${trk_name}.trk"
 
-    #python3 $generate_masks $trk_fn ${output_dir}/${tract_name}.nii.gz $ref_affine_file $is_legacy
+    # Since all HCP data shares same affine, can use any subject as the affine reference
+    ref_affine_file="../../../DATASETS/HCP_100_SUBJECTS/100307/T1w/Diffusion/nodif_brain_mask.nii.gz" 
+
+    ### GENERATE A TRACT MASK ###
+    is_legacy=1 # set to 1 if using legacy data (dataset <=V1.1.0), else set to 0
+    python3 $generate_masks $trk_fn ${output_masks_base}/${trk_name}.nii.gz $ref_affine_file $is_legacy
 
     # Generate endings (beginning/end) masks
-    #ref_affine_file="../../../DATASETS/HCP_100_SUBJECTS/100307/T1w/Diffusion/nodif_brain_mask.nii.gz" 
-    #trk_fn= the actual trk file we want to get an endings mask for
-    #python3 $generate_endings $ref_affine_file $trk_fn ${output_dir}/${output_fn}
+    python3 $generate_endings $ref_affine_file $trk_fn ${output_endings_base}/${trk_name}
 
     # Generate a TOM
+    mask="${output_masks_base}/${trk_name}.nii.gz"
+    sh $generate_toms -i $trk_fn -o ${output_toms_base}/$trk_name --mask $mask --athresh 10 --peakthresh 0.1 --numdirs 1 --normalization 3 --file_ending .nii.gz
 
     # Pre-process the generated nifti files
+done
