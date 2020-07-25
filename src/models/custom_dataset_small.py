95a6bac1acf902c540f47e062de2a4ab2039125b
aritche <11988281+aritche@users.noreply.github.com>
2020-07-25 23:16:50 +1000

    very small model

--- src/models/custom_dataset_small.py
+++ src/models/custom_dataset_small.py
@@ -30,33 +30,16 @@ class CustomModel(nn.Module):
                                                                                                                  # VOLUME SIZE                      # PARAMETERS
         # Encoding (input -> 512 vector)                                                                         # 3 x 144 x 144 x 144 -> 8.9M      (IN * F^3 + 1)*OUT
 
+        # num_points*3, 8, 8
         self.down_conv_1 = nn.Conv3d(in_channels=3, out_channels=3, kernel_size=(3,3,3), stride=1, padding=0)     # K   x 136 x 136 x 136 ->        3*(3*9^3+1)     = 6.5K
         self.down_conv_2 = nn.Conv3d(in_channels=3, out_channels=44, kernel_size=(4,4,4), stride=3, padding=0) # K   x 64  x 64  x 64  ->        44*(3*10^3+1)   = 132K
         self.down_conv_3 = nn.Conv3d(in_channels=44, out_channels=32, kernel_size=(3,3,3), stride=2, padding=1)   # K   x 32  x 32  x 32  ->        32*(44*6^3+1)   = 305K
         self.down_conv_4 = nn.Conv3d(in_channels=32, out_channels=32, kernel_size=(3,3,3), stride=3, padding=0)   # K   x 16  x 16  x 16  ->        76*(32*6^3+1)   = 525K
-        self.down_conv_5 = nn.Conv3d(in_channels=32, out_channels=32, kernel_size=(4,4,4), stride=2, padding=0)
-        self.down_conv_6 = nn.Conv3d(in_channels=32, out_channels=512, kernel_size=(3,3,3), stride=1, padding=0)
-        #self.down_conv_5 = nn.Conv3d(in_channels=64, out_channels=512, kernel_size=(4,4,4), stride=2, padding=1)  # K   x 16  x 16  x 16  ->        288*(76*3^3+1)  = 590K
-        #self.down_conv_6 = nn.Conv3d(in_channels=288, out_channels=150, kernel_size=(4,4,4), stride=2, padding=1) # K   x 8   x 8   x 8   ->        150*(288*4^3+1) = 2.7M
-        #self.down_conv_7 = nn.Conv3d(in_channels=150, out_channels=512, kernel_size=(3,3,3), stride=1, padding=1) # K   x 8   x 8   x 8   ->        512*(150*3^3+1) = 2.1M
-        #self.down_pool_8 = nn.MaxPool3d(kernel_size=8)                                                            # 512 x 1   x 1   x 1                  
 
-        # Decoding (512 vector -> 32 x 32 x 512 volume)
-        self.up_linear_1 = nn.Linear(in_features=512, out_features=1024)
-        self.up_linear_2 = nn.Linear(in_features=1024, out_features=4096)
-        self.up_conv_3 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1)
-        self.up_conv_4 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1)
-        self.up_conv_5 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1)
 
-        # Position decoder (32 x 32 x 512 volume -> 32 x 32 x 300)
-        self.pos_conv_1 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=1, stride=1, padding=0)
-        self.pos_conv_2 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=1, stride=1, padding=0)
-        self.pos_conv_3 = nn.Conv2d(in_channels=512, out_channels=num_points*3, kernel_size=1, stride=1, padding=0)
-
-        # Seed decoder (32 x 32 x 512 volume -> 32 x 32 x 3)
-        self.seed_conv_1 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=1, stride=1, padding=0)
-        self.seed_conv_2 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=1, stride=1, padding=0)
-        self.seed_conv_3 = nn.Conv2d(in_channels=512, out_channels=3, kernel_size=1, stride=1, padding=0)
+        # num_points*3 x 8 x 8 x 8 
+        self.down_conv_5a = nn.Conv3d(in_channels=32, out_channels=num_points*3, kernel_size=(2,2,2), stride=2, padding=0)   # K   x 16  x 16  x 16  ->        76*(32*6^3+1)   = 525K
+        self.down_conv_5b = nn.Conv3d(in_channels=32, out_channels=3, kernel_size=(2,2,2), stride=2, padding=0)   # K   x 16  x 16  x 16  ->        76*(32*6^3+1)   = 525K
 
     def forward(self, x):
         # Encoding
@@ -66,44 +49,13 @@ class CustomModel(nn.Module):
         x = self.relu(x)
         x = self.down_conv_3(x) # Output: (32, 24, 24, 24)
         x = self.relu(x)
-        x = self.down_conv_4(x) # Output: (512, 8, 8, 8)
-        x = self.relu(x)
-        x = self.down_conv_5(x) # Output: (512, 3, 3, 3)
-        x = self.relu(x)
-        x = self.down_conv_6(x) # Output: (512, 1, 1, 1)
-        # Pooling
-        #x = self.down_pool_8(x) # Output: (512, 1, 1, 1)
+        x = self.down_conv_4(x) # Output: (32, 8, 8, 8)
         x = self.tanh(x)
 
-        x = x.view(-1, 512)     # Output: (512)
-
-        # Decoding
-        x = self.up_linear_1(x) # Output: (1024)
-        x = self.relu(x)
-        x = self.up_linear_2(x) # Output: (4096)
-        x = self.relu(x)
-
-        x = x.view(-1, 256, 4, 4) # Output: (256, 4, 4)
-        x = self.upsample(x)      # Output: (256, 8, 8)
-
-        x = self.up_conv_3(x)     # Output: (512, 8, 8)
-        x = self.relu(x)
-        x = self.up_conv_4(x)     # Output: (512, 8, 8)
-        x = self.relu(x)
-        x = self.up_conv_5(x)     # Output: (512, 8, 8)
-        x = self.relu(x)
-
-        p = self.pos_conv_1(x)    # Output: (512, 8, 8)
-        p = self.relu(p)
-        p = self.pos_conv_2(p)    # Output: (512, 8, 8)
-        p = self.tanh(p)
-        p = self.pos_conv_3(p)    # Output: (num_points*3, 32, 32)
-
-        s = self.seed_conv_1(x)    # Output: (512, 32, 32)
-        s = self.relu(s)          
-        s = self.seed_conv_2(s)    # Output: (512, 32, 32)
-        s = self.tanh(s)
-        s = self.seed_conv_3(s)    # Output: (3, 32, 32)
+        p = self.down_conv_5a(x)
+        p = p.view(-1, num_points*3, 8, 8)
+        s = self.down_conv_5b(x)
+        s = s.view(-1, 3, 8, 8)
 
         return [s, p]
 
